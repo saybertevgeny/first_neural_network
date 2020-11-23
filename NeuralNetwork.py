69181786a48092a1d1b4ff7234c6bf82bccb1eb7
Saybert <saybert@lanit.ru>
2020-11-24 01:08:35 +0300

    Initialization of matrix of weighting coefficients

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,7 +1,16 @@
+import numpy
+
 class NeuralNetwork:
 
-    def __init(self):
-        pass
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
+        self.input_nodes = input_nodes
+        self.hidden_nodes = hidden_nodes
+        self.output_nodes = output_nodes
+        self.learning_rate = learning_rate
+        #матрица весовых коэфициентов от входного к скрытому слою
+        self.wih = numpy.random.rand(self.hidden_nodes, self.input_nodes) - 0.5
+        #матрица весовых коэфициентов от скрытого к выходному слою
+        self.woh = numpy.random.rand(self.output_nodes, self.hidden_nodes) - 0.5
 
     def train(self):
         pass
